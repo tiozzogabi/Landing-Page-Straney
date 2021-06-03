0c6f4a6ea7069862e9e0bf8dffcdf57c1e36e030
Gabrielle Tiozzo <tiozzogabi@gmail.com>
2021-06-03 10:10:23 -0300

    starting changes

---
+++ src/js/nav.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.fa')
+const nav = document.querySelector('.menu')
+console.log(nav)
+
+menuBtn.addEventListener("click", () =>{
+  nav.classList.toggle("responsive")
+
+})
+
